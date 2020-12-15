600c728ba752f9363206ba81d699477751137093
Devin Dao-Tse <devindaotse@gmail.com>
2020-12-15 15:56:07 -0500

    add data to db, need to test

---
+++ seeds/index.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const campgrounds = require('../models/campgrounds')
+const Campground = require('../models/campgrounds')
+const {places, descriptors} = require('./seedHelper')
+const cities = require('./cities')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true
+})
+// .then(()=>{
+//     console.log('CONNECTION OPEN')
+//   }).catch(err=>{
+//     console.log('CANT CONNECT')
+//     console.log(err)
+//   })
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, 'connection error'))
+db.once("open",()=>{
+    console.log('Database connected')
+})
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+const seedDB = async ()=>{
+    await Campground.deleteMany({})
+    const c = new Campground({title: 'Purple field'})
+    await c.save()
+    for(let i =0;i<50; i++){
+        const random100 = Math.floor(Math.random()*100)
+        const camp = new Campground({
+            location: `${cities[random100].city}, ${cities[random100].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save()
+    }
+}
+
+seedDB().then(()=> {
+    mongoose.connection.close
+})
