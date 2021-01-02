b7499db64516e99ef9fd2ddf28a1a4b9d0c8cf52
Devin Dao-Tse <devindaotse@gmail.com>
2021-01-02 16:50:34 -0500

    created a review page

---
+++ models/review.js
@@ -0,0 +1,14 @@
+const { number } = require('joi')
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const reviewSchema = new Schema({
+    body: String,
+    rating: Number
+})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = {
+    Review: Review
+}
